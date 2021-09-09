456ca99f91721c91eba9e7e4fed3e2501115587e
akshaypatel <akshaypatel989@gmail.com>
2021-09-09 09:08:44 +0100

    Add features using custom pipe dfor height and weight loading not working !

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
   }
-
 }
+
+
+
