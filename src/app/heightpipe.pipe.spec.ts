f68aac9d77fd7b85db102cc1f72b8082e11e8aab
akshaypatel <akshaypatel989@gmail.com>
2021-09-09 09:15:37 +0100

    Add features using custom pipe dfor height and weight loading not working !

---
+++ src/app/heightpipe.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { HeightpipePipe } from './heightpipe.pipe';
+
+describe('HeightpipePipe', () => {
+  it('create an instance', () => {
+    const pipe = new HeightpipePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
